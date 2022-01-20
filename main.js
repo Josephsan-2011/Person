12536212866e29d606e1cbe8599376c1b60b55ba
Josephsan-2011 <83101904+Josephsan-2011@users.noreply.github.com>
2022-01-19 18:45:37 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//https://teachablemachine.withgoogle.com/models/l6B8qc6Id/
+//https://teachablemachine.withgoogle.com/models/v9B0pEMJd/
 Webcam.set({
     width: 350,
     height: 300,
@@ -14,7 +14,21 @@ function snapshot() {
     })
 }
 console.log(ml5.version)
-var classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/l6B8qc6Id/model.json',model_loaded)
+var classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v9B0pEMJd/model.json',model_loaded)
 function model_loaded() {
     console.log('model_loaded')
+}
+function check() {
+    img = document.getElementById('capture_image')
+    classifier.classify(img,gotresult)
+}
+function gotresult(error,results) {
+    if (error) {
+        console.error(error)
+    }
+    else {
+        console.log(results)
+        document.getElementById("object_name").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(2);
+    }
 }
